04bc429556f4349aae989b2f88655adb7d1fef80
cotalora_ <67478427+cotalora@users.noreply.github.com>
2022-02-16 08:07:23 -0500

    Upload project

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function selectJob(event, jobName) {
+    let element = document.getElementById(jobName);
+    let jobSections = document.getElementsByClassName('job');
+    let jobButtons = document.getElementsByClassName('button-job');
+    console.log(event);
+    let button = event.currentTarget;
+
+    // Ocultar/Mostrar sección de trabajo
+    for (let i = 0; i < jobSections.length; i++) {
+        if (!jobSections[i].classList.contains('hidden')) {
+            jobSections[i].classList.add('hidden');
+            jobSections[i].classList.add('visuallyhidden');
+        }
+    }
+    element.classList.remove('hidden');
+    setTimeout(function () {
+        element.classList.remove('visuallyhidden');
+    }, 20);
+
+    // Deshabilitar/Habilitar botón
+    for (let i = 0; i < jobButtons.length; i++) {
+        if (jobButtons[i].classList.contains('button-job-selected')) {
+            jobButtons[i].classList.remove('button-job-selected');
+        }
+    }
+    button.classList.add('button-job-selected');
+}
